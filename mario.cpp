8760f452961d14befc814d8986f68f93a248d67a
Leon301123 <wei0326leon@gmail.com>
2024-11-20 23:03:11 +0800

    Add files via upload

---
+++ mario.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n;
+cout << "n=? ";
+cin >> n ;
+  for (int f = 1 ; f<=n ; f++){
+	    for (int e = n-1 ; e>=f ; e--){
+			  cout << " ";
+		  }
+		  for (int j = 1; j <= f ; j++) {
+			cout << "#";
+		  }
+    cout << " " ;
+
+		  for (int i = 1 ; i<=f ;i++){
+			  cout << "#";
+		  }
+		cout << "\n" ;
+	}
+}
